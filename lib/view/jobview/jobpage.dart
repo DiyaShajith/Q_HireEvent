7b1987ed80529888994625f199eeb56568d895aa
Diya Shajith K <diyashajith.2231@gmail.com>
2024-09-03 08:59:39 +0530

    savejob completed

--- lib/view/jobview/jobpage.dart
+++ lib/view/jobview/jobpage.dart
@@ -9,8 +9,23 @@ class Jobpage extends StatefulWidget {
 }
 
 class _JobpageState extends State<Jobpage> {
+  // Explicitly defining the type of the lists as List<String>
+  final List<String> image = [
+    "assets/microsoft.png",
+    "assets/twitter.png",
+    "assets/facebook.png"
+  ];
+  final List<String> title = ["Microsoft", "Twitter", "Facebook"];
+  final List<String> subtitle = [
+    "Product Designer",
+    "Product Designer",
+    "Product Designer"
+  ];
+  final List<String> leadingicon = ["assets/save.png"];
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
     return Scaffold(
       backgroundColor: CustomColor.scaffoldbg,
       body: SafeArea(
@@ -20,40 +35,129 @@ class _JobpageState extends State<Jobpage> {
             Row(
               children: [
                 IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.arrow_back,
-                      color: CustomColor.blackprimary,
-                    )),
-                Text(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: CustomColor.blackprimary,
+                  ),
+                ),
+                const Text(
                   "Saved Jobs",
                   style: TextStyle(
-                      color: CustomColor.blackprimary,
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold),
-                )
+                    color: CustomColor.blackprimary,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ],
             ),
             const SizedBox(
               height: 20,
             ),
-            TextField(
-              decoration: InputDecoration(
-                  hintText: "Job title,Keyword, or Company",
-                  hintStyle: TextStyle(
+            Container(
+              width: size.width,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    blurRadius: 20,
+                    spreadRadius: 1,
+                    offset: const Offset(0, 1),
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                    suffixIcon: const Icon(
+                      Icons.mic_outlined,
+                      color: CustomColor.textfieldbg,
+                      size: 28,
+                    ),
+                    hintText: "Job title, Keyword, or Company",
+                    hintStyle: const TextStyle(
                       color: CustomColor.uploadbg,
                       fontSize: 16,
-                      fontWeight: FontWeight.w500),
-                  fillColor: CustomColor.scaffoldbg,
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide.none,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    fillColor: CustomColor.scaffoldbg,
+                    filled: true,
+                    prefixIcon: const Icon(
+                      Icons.search,
+                      color: CustomColor.textfieldbg,
+                      size: 28,
+                    ),
                   ),
-                  prefixIcon: const Icon(
-                    Icons.search,
-                    color: CustomColor.textfieldbg,
-                  )),
-            )
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            ListView.separated(
+              itemBuilder: (context, index) => Container(
+                padding: const EdgeInsets.all(16),
+                width: size.width,
+                height: size.height * 0.1,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  border: Border.all(
+                    color: CustomColor.textfieldbg1,
+                    width: 0.5,
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      image[index],
+                      width: 55,
+                      height: 55,
+                    ),
+                    const SizedBox(width: 16),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          title[index],
+                          style: const TextStyle(
+                            color: CustomColor.blackprimary,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          subtitle[index],
+                          style: const TextStyle(
+                            color: CustomColor.uploadbg,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Spacer(),
+                    Image.asset(
+                      leadingicon[0],
+                      width: 24,
+                      height: 24,
+                    ),
+                  ],
+                ),
+              ),
+              separatorBuilder: (context, index) => const SizedBox(
+                height: 20,
+              ),
+              shrinkWrap: true,
+              itemCount: image.length,
+            ),
           ],
         ),
       ),
