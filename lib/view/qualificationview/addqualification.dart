6f5a0e11fa73b24613ce380f4127c8a450dedbeb
Diya Shajith K <diyashajith.2231@gmail.com>
2024-09-03 20:14:00 +0530

    Addqualificationcompleted

--- lib/view/qualificationview/addqualification.dart
+++ lib/view/qualificationview/addqualification.dart
@@ -9,18 +9,27 @@ class Addqualification extends StatefulWidget {
 }
 
 class _AddqualificationState extends State<Addqualification> {
+  final List<String> labels = [
+    "Education",
+    "University/College",
+    "Enrolled Year",
+    "Pass out Year"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: CustomColor.scaffoldbg,
       appBar: AppBar(
         backgroundColor: CustomColor.scaffoldbg,
         leading: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.arrow_back,
-              color: CustomColor.blackprimary,
-              size: 32,
-            )),
+          onPressed: () {},
+          icon: Icon(
+            Icons.arrow_back,
+            color: CustomColor.blackprimary,
+            size: 32,
+          ),
+        ),
         title: const Text(
           "Add Qualification",
           style: TextStyle(
@@ -29,7 +38,62 @@ class _AddqualificationState extends State<Addqualification> {
               fontWeight: FontWeight.bold),
         ),
       ),
-      backgroundColor: CustomColor.scaffoldbg,
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.separated(
+                itemCount: labels.length,
+                itemBuilder: (context, index) {
+                  return TextField(
+                    decoration: InputDecoration(
+                      label: Text(
+                        labels[index],
+                        style: const TextStyle(
+                          color: CustomColor.uploadbg,
+                          fontSize: 19,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: BorderSide(
+                          color: CustomColor.textfieldbg1,
+                          width: 0.5,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) =>
+                    const SizedBox(height: 20),
+              ),
+            ),
+            SizedBox(
+              width: 350,
+              height: 60,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  backgroundColor: CustomColor.textfieldbg,
+                ),
+                onPressed: () {},
+                child: const Text(
+                  "Update",
+                  style: TextStyle(
+                    color: CustomColor.scaffoldbg,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
